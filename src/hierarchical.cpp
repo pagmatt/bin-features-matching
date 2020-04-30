6f21180a6cc87bb47b21b28f8731138645032494
pagmatt <mattpagg@gmail.com>
2020-04-30 12:44:49 +0200

    Better profiling

--- src/hierarchical.cpp
+++ src/hierarchical.cpp
@@ -12,11 +12,11 @@ using namespace std;
 // OpenCV
 using namespace cv;
 
-int const feat_to_compute = 50;
+int const feat_to_compute = 100;
 int const max_features_to_search = 25;
 int const branching_factor = 5;
-int const max_leaves_amount = 5;
-int const trees_amount = 2;
+int const max_leaves_amount = 20;
+int const trees_amount = 1;
 int const top_k_feat = 2;	// Extract just top 2, enough in order to use NNDR technique
 int const px_to_draw = 100;
 int const gap = 30;
@@ -29,6 +29,12 @@ find_ORB_matches (cv::Mat &src, cv::Mat &dest);
 
 cv::Mat 
 find_SIFT_matches (cv::Mat &src, cv::Mat &dest);
+
+void
+profile_orb(cv::Mat &src, cv::Mat &dest, bool lin);
+
+void
+profile_sift(cv::Mat &src, cv::Mat &dest);
 
 int main(int, char **)
 {
@@ -48,12 +54,19 @@ int main(int, char **)
         return -1;
     }
 
+	// Compute matches
+
+	/*
 	cv::Mat orb_matches = find_ORB_matches(src, dest);
 	cv::Mat sift_matches = find_SIFT_matches(src, dest);
 	// Features computation : ORB comparison
 
 	cv::imwrite("../orb_matches.jpeg", orb_matches);
 	cv::imwrite("../sift_matches.jpeg", sift_matches);
+	*/
+
+	// Profile code
+	profile_orb(src, dest, false);
 
 }
 
@@ -127,7 +140,7 @@ find_ORB_matches (cv::Mat &src, cv::Mat &dest)
 		if(dest_kp.x - px_to_draw/2 < 0 || dest_kp.y - px_to_draw/2 < 0 || 
 			dest_kp.x + px_to_draw/2 > dest.rows || dest_kp.y + px_to_draw/2 > dest.cols)
 		{
-			std::cout << "Skipping feature as its keypoint exceeds image bounds" << std::endl;
+			skipped++;
 			continue;
 		}
 		cv::Mat dest_crop = dest(dst_to_crop);
@@ -256,6 +269,69 @@ find_SIFT_matches (cv::Mat &src, cv::Mat &dest)
 	return stacked_sift;
 }
 	
+void 
+profile_orb(cv::Mat &src, cv::Mat &dest, bool lin)
+{
+	Ptr<ORB> src_orb_obj = ORB::create(feat_to_compute);
+	vector<KeyPoint>src_orb_points;
+	src_orb_obj->detect(src, src_orb_points);
+	//cout << "Detected " << orb_points.size() << " keypoints!" << endl; 
+	cv::Mat src_out_orb_feat;
+	src_orb_obj->compute(src, src_orb_points, src_out_orb_feat);
+	//cout << "Computed " << out_orb_feat.size().height << " features!" << endl; 
+
+	Ptr<ORB> dest_orb_obj = ORB::create(feat_to_compute);
+	vector<KeyPoint>dest_orb_points;
+	dest_orb_obj->detect(dest, dest_orb_points);
+	//cout << "Detected " << orb_points.size() << " keypoints!" << endl; 
+	cv::Mat dest_out_orb_feat;
+	dest_orb_obj->compute(dest, dest_orb_points, dest_out_orb_feat);
+		
+	
+	for(int j = 0; j < feat_to_compute; j++)
+	{
+		if(lin)
+		{
+			MatchingLibs::linear_search(dest_out_orb_feat, src_out_orb_feat.row(j));
+		}
+		else
+		{
+			MatchingLibs::parallel_search(dest_out_orb_feat, branching_factor, max_leaves_amount, trees_amount, 
+													max_features_to_search, top_k_feat, src_out_orb_feat.row(j));
+		}
+		
+	}
+}
 
 
+void 
+profile_sift(cv::Mat &src, cv::Mat &dest)
+{
+	Ptr<SIFT> src_sift_obj = SIFT::create(feat_to_compute);
+	vector<KeyPoint>src_sift_points;
+	src_sift_obj->detect(src, src_sift_points);
+	//cout << "Detected " << src_sift_points.size() << " keypoints!" << endl; 
+	cv::Mat src_out_sift_feat;
+	src_sift_obj->compute(src, src_sift_points, src_out_sift_feat);
+	//cout << "Computed " << src_out_sift_feat.size().height << " features!" << endl; 
+
+	Ptr<SIFT> dest_sift_obj = SIFT::create(feat_to_compute);
+	vector<KeyPoint>dest_sift_points;
+	dest_sift_obj->detect(dest, dest_sift_points);
+	//cout << "Detected " << orb_points.size() << " keypoints!" << endl; 
+	cv::Mat dest_out_sift_feat;
+	dest_sift_obj->compute(dest, dest_sift_points, dest_out_sift_feat);
+
+	//Rows = #features, cols = # components, type is 32F
+	//std::cout << CV_MAT_TYPE(dest_out_sift_feat.type()) << std::endl;
+	//Quantize the features
+	cv::Mat quantized_dest_sift = MatchingLibs::median_quantize(dest_out_sift_feat);
+	cv::Mat quantized_src_sift = MatchingLibs::median_quantize(src_out_sift_feat);
+
+	for(int j = 0; j < feat_to_compute; j++)
+	{
+		MatchingLibs::parallel_search(quantized_dest_sift, branching_factor, max_leaves_amount, trees_amount, 
+													max_features_to_search, top_k_feat, quantized_src_sift.row(j));
+	}
+}	
 
